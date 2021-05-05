51f80f51e29e92c9c7677eb5d83018ab6d1c3f17
zmodir <modir@techranco.com>
2021-05-05 18:00:47 +0430

    starting koin

--- app/src/main/java/ir/nabzi/aroundme/App.kt
+++ app/src/main/java/ir/nabzi/aroundme/App.kt
@@ -1,20 +1,24 @@
 package ir.nabzi.aroundme
 
 import android.app.Application
+import ir.nabzi.aroundme.di.appModule
+import ir.nabzi.aroundme.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 
 class App : Application() {
     override fun onCreate(){
         super.onCreate()
-//        startKoin {
-//            androidContext(this@App)
-//            modules(
-//                listOf(
-//                    appModule,
-//                    networkModule,
-//                    dbModule
-//                )
-//            )
-//        }
+        startKoin {
+            androidContext(this@App)
+            modules(
+                listOf(
+                    appModule,
+                    networkModule,
+                    dbModule
+                )
+            )
+        }
     }
 }
