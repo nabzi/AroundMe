524f1d60e7fde950e75c29a0f5db9b9dcaed45c9
zmodir <modir@techranco.com>
2021-05-05 17:30:27 +0430

    Added Repository

---
+++ app/src/main/java/ir/nabzi/aroundme/repository/PlaceRepository.kt
@@ -0,0 +1,73 @@
+package ir.nabzi.aroundme.data.repository
+
+import com.google.gson.Gson
+import ir.nabzi.places.data.db.PlaceDao
+import ir.nabzi.places.data.remote.ApiService
+import ir.nabzi.places.ir.nabzi.places.data.repository.RemoteResource
+import ir.nabzi.places.model.NetworkCall
+import ir.nabzi.places.model.Place
+import ir.nabzi.places.model.Resource
+import ir.nabzi.places.model.Status
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+interface PlaceRepository {
+    suspend fun getPlaces(
+        shouldFetch: Boolean
+    ): StateFlow<Resource<List<Place>>?>
+}
+
+class PlaceRepositoryImpl(
+    val placeDao: PlaceDao,
+    val apiServices: ApiService
+) : PlaceRepository {
+
+    override suspend fun getPlaces(shouldFetch: Boolean): StateFlow<Resource<List<Place>>?> {
+        var stateFlow: MutableStateFlow<Resource<List<Place>>?> =
+            MutableStateFlow(Resource.loading(null))
+        stateFlow.emit(
+            object : RemoteResource<List<Place>>() {
+                override suspend fun updateDB(result: List<Place>) {
+                    updateDBSource(result)
+                }
+
+                override fun getFromDB(): List<Place> {
+                    return getPlacesFromDBSource()
+                }
+
+                override suspend fun pullFromServer(): Resource<List<Place>> {
+                    return getPlacesFromRemoteSource()
+                }
+            }.get(true)
+        )
+        return stateFlow
+    }
+
+    /**Data Sources:
+     * This functions can be placed in classes
+     * PlaceDBSource and PlaceRemoteSource if they are large
+     * and added as dependency for repository
+    **/
+    private suspend fun updateDBSource(result : List<Place>) {
+        placeDao.addList(result)
+    }
+
+    private fun getPlacesFromDBSource(): List<Place> {
+        return placeDao.getPlaces()
+    }
+
+    private suspend fun getPlacesFromRemoteSource(): Resource<List<Place>> {
+        return object : NetworkCall<List<Place>>() {
+            override suspend fun createCall(): Response<List<Place>> {
+                return apiServices.getPlaceList()
+            }
+        }.fetch()
+    }
+
+}
---
+++ app/src/main/java/ir/nabzi/aroundme/repository/RemoteResource.kt
@@ -0,0 +1,40 @@
+package ir.nabzi.aroundme.data.repository
+
+import ir.nabzi.places.model.Place
+import ir.nabzi.places.model.Resource
+import ir.nabzi.places.model.Status
+
+/**
+*   RemoteResource<ResultType> is an abstract class that can be used for resources that can be fetched
+ *   from remote server or database.
+ *   If parameter "shouldFetch" is set to true, we always fetch data from server and update database,
+ *   and it can be set to "data == nullOrEmpty()" if we want to load data from remote server if
+ *   data is not present in database yet.
+* */
+abstract class RemoteResource<ResultType> {
+    suspend fun get(shouldFetch : Boolean): Resource<ResultType>{
+        return if (shouldFetch) {
+            val resource = pullFromServer()
+
+            if (resource.status == Status.ERROR) {
+                Resource.error<ResultType>(
+                    resource.message ?: "error loading from server", getFromDB()
+                )
+            } else {
+                resource.data?.let {
+                    updateDB(it)
+                }
+                Resource.success(getFromDB())
+            }
+
+        } else {
+            Resource.success(getFromDB())
+        }
+    }
+
+    abstract suspend fun updateDB(result : ResultType)
+
+    abstract fun getFromDB(): ResultType
+
+    abstract suspend fun pullFromServer(): Resource<ResultType>
+}
