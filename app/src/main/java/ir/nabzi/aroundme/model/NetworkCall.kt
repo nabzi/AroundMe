ef6b29556499ce4b15a241026b6d450055f58d86
zmodir <modir@techranco.com>
2021-05-05 17:24:35 +0430

    Added retrofir and network classes

---
+++ app/src/main/java/ir/nabzi/aroundme/model/NetworkCall.kt
@@ -0,0 +1,69 @@
+package ir.nabzi.aroundme
+
+import Resource
+import okhttp3.Headers
+import org.json.JSONException
+import org.json.JSONObject
+import retrofit2.HttpException
+import retrofit2.Response
+import java.net.ConnectException
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
+/**
+ *   NetworkCall<ResultType> is an abstract class that can be used for api calls.
+ *   It manages error cases and returns proper error message and error code
+ *   */
+abstract class NetworkCall<ResultType> {
+    suspend fun fetch(): Resource<ResultType> {
+        return try {
+            val response = createCall()
+            if (response.isSuccessful) {
+                onSuccess(response.body())
+                Resource(Status.SUCCESS, response.body(),"success")
+
+            } else{
+                val errorMsg = response.errorBody()?.let {
+                    JSONObject(it.charStream().readText()).getString("message")
+                }
+
+                Resource.error(errorMsg?:"" ,null, response.code())
+            }
+
+        } catch (e: HttpException) {
+            e.printStackTrace()
+            Resource.error("", null, e.code())
+
+        } catch (e: ConnectException) {
+            e.printStackTrace()
+            Resource.error("Network Connection error",null, ConnectException)
+
+        } catch (e: SocketTimeoutException) {
+            e.printStackTrace()
+            Resource.error( "Network Connection error",null, SocketTimeoutException)
+
+        }catch (e : UnknownHostException){
+            e.printStackTrace()
+            Resource.error("Network Connection error",null, UnknownHostException)
+
+        }catch (e : JSONException){
+            e.printStackTrace()
+            Resource.error("",null, JSONException)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Resource.error("",null, Exception)
+        }
+    }
+
+    abstract suspend fun createCall(): Response<ResultType>
+    open suspend fun onSuccess(result: ResultType?){}
+    companion object{
+        final const val ConnectException=600
+        final const val SocketTimeoutException=601
+        final const val UnknownHostException=602
+        final const val Exception=603
+        final const val JSONException=604
+    }
+}
---
+++ app/src/main/java/ir/nabzi/aroundme/remote/retrofit.kt
@@ -0,0 +1,34 @@
+package ir.nabzi.aroundme.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+//https://mockapi.io/projects/6086fa75a3b9c200173b758f
+
+const val BASE_URL = "https://6086fa75a3b9c200173b758e.mockapi.io"
+
+fun createHttpClient(): OkHttpClient {
+    val okHttpClientBuilder = OkHttpClient.Builder()
+    if (BuildConfig.DEBUG) {
+        val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        okHttpClientBuilder.addInterceptor(httpLoggingInterceptor)
+    }
+    return okHttpClientBuilder.build()
+}
+
+fun createRetrofit(client: OkHttpClient, baseUrl: String): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+}
+
+fun createService(retrofit: Retrofit): ApiService {
+    return retrofit.create(ApiService::class.java)
+}
+
