d30878dc06dee137c72468a98830923fec14678c
zmodir <modir@techranco.com>
2021-05-05 17:38:41 +0430

    Added mapbox token

--- app/src/main/java/ir/nabzi/aroundme/ui/Constants.kt
+++ app/src/main/java/ir/nabzi/aroundme/ui/Constants.kt
@@ -2,4 +2,4 @@ package ir.nabzi.aroundme.ir.nabzi.aroundme.ui
 
 
 const val MAP_ACCESS_TOKEN =
-    "sk.eyJ1IjoibmFiemkiLCJhIjoiY2tueGFuMWJyMTRqMTJ2cW42NjUya3dzaSJ9.0uj9WisAz4Xd18I8s5rW9g"
+"sk.eyJ1IjoibmFiemkiLCJhIjoiY2tvYmg2dTh1MWVhcDJvbXFyODF0ajhqcyJ9.OYawCinnp3ZCEXDB_3MZ-A"
