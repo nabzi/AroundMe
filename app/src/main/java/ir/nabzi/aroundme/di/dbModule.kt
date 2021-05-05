76ec46bbfbe07e2b4ed153eedb28435c3bc26c29
zmodir <modir@techranco.com>
2021-05-05 17:22:01 +0430

    Added di and model

---
+++ app/src/main/java/ir/nabzi/aroundme/di/dbModule.kt
@@ -0,0 +1,20 @@
+package ir.nabzi.aroundme
+
+import android.app.Application
+import androidx.room.Room
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+val dbModule = module {
+    fun provideDatabase(application: Application): DB {
+        return Room.databaseBuilder(application, DB::class.java, "place").build()
+    }
+
+    fun providePlaceDao(database: DB): PlaceDao {
+        return database.placeDao()
+    }
+
+    single { provideDatabase(androidApplication()) }
+
+    single { providePlaceDao(get()) }
+}
---
+++ app/src/main/java/ir/nabzi/aroundme/model/Place.kt
@@ -0,0 +1,21 @@
+package ir.nabzi.aroundme
+
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "place")
+@Parcelize
+data class Place
+    (
+    @PrimaryKey
+    val id:         String,
+    val title:      String,
+    val subTitle:   String,
+    val imageUrl:   String,
+    val rating:     Int,
+    val location_lat :   Double,
+    val location_lng :   Double
+) : Parcelable
