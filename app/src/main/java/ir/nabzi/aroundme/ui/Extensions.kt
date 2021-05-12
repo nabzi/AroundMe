b9d819d666b92d791f21f9237552e7916e737390
zmodir <modir@techranco.com>
2021-05-12 11:03:13 +0430

    Added save location functions

--- app/src/main/java/ir/nabzi/aroundme/ui/Extensions.kt
+++ app/src/main/java/ir/nabzi/aroundme/ui/Extensions.kt
@@ -1,5 +1,6 @@
 package ir.nabzi.aroundme.ir.nabzi.aroundme.ui
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
@@ -7,6 +8,7 @@ import android.location.LocationManager
 import android.widget.Toast
 import androidx.core.location.LocationManagerCompat
 import androidx.fragment.app.Fragment
+import com.mapbox.mapboxsdk.geometry.LatLng
 
 
 fun Fragment.showError(message: String?) {
@@ -26,4 +28,17 @@ fun Context.requireLocationEnabled() {
 fun Context.locationEnabled(): Boolean {
     val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
     return LocationManagerCompat.isLocationEnabled(locationManager)
+}
+fun Activity.saveLocation(latLng: LatLng){
+    val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return
+    with (sharedPref.edit()) {
+        putFloat("lat", latLng.latitude.toFloat())
+        putFloat("lon", latLng.longitude.toFloat())
+        apply()
+    }
+}
+
+fun Activity.getLastLocation(): LatLng? {
+    val sharedPref = getPreferences(Context.MODE_PRIVATE) ?: return null
+    return  LatLng(sharedPref.getFloat("lat", 0f).toDouble() , sharedPref.getFloat("lon", 0f).toDouble())
 }
